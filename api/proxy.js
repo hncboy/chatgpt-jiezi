b752affe010bd6db9bc88c7c45b170d38fcdcceb
mjjh <1692762315@qq.com>
2023-04-05 10:10:03 +0800

    wip: 测试vercel跨域配置

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,14 @@
 // api/proxy.js
 // 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const viteBaseUrl = process.env.VITE_BASE_URL
 
-module.exports = (req, res) => {
-  let target = ''
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  // target 替换为你跨域请求的服务器 如： http://xxxxxx.cn
-  if (req.url.startsWith('/api'))
-    target = 'https://api-demo.stargpt.top/admin'
-
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
+module.exports = function (app) {
+  app.use('/api', createProxyMiddleware({
+    target: viteBaseUrl,
     changeOrigin: true,
     pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 如果开启了,那么 /api/user/login 将被转发到 http://xxxxx.cn/user/login
-      '^/api/': '/',
+      '^/api': '',
     },
-  })(req, res)
+  }))
 }
