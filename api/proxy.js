ac14922d6ffae3d0022bbc6ec653efc793227323
mjjh <1692762315@qq.com>
2023-04-05 10:15:23 +0800

    wip: 测试vercel跨域配置

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,17 @@
 // api/proxy.js
 // 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const viteBaseUrl = process.env.VITE_BASE_URL
+const backendApiUrl = process.env.VITE_BASE_URL
 
-module.exports = function (app) {
-  app.use('/api', createProxyMiddleware({
-    target: viteBaseUrl,
+module.exports = (req, res) => {
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    backendApiUrl,
     changeOrigin: true,
     pathRewrite: {
-      '^/api': '',
+      // 通过路径重写，去除请求路径中的 `/api`
+      // 如果开启了,那么 /api/user/login 将被转发到 http://xxxxx.cn/user/login
+      '^/api/': '',
     },
-  }))
+  })(req, res)
 }
